6b188584869ca56c01585a6160a6e4ddb747d8b8
Suar <csuar-2017085@kinal.edu.gt>
2022-09-12 20:53:41 -0600

    Se acreó el archivo Factura.java al paquete modelo del proyecto Venta_Autos_Repuesto_Chapin

---
+++ Venta_Autos_Repuesto_Chapin/src/java/modelo/Factura.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author juani
+ */
+public class Factura {
+    private int idFactura;
+    private String fechaFactura;
+    private int codCliente;
+    private String id;
+
+    public Factura() {
+    }
+
+    public Factura(int idFactura, String fechaFactura, int codCliente, String id) {
+        this.idFactura = idFactura;
+        this.fechaFactura = fechaFactura;
+        this.codCliente = codCliente;
+        this.id = id;
+    }
+
+    public int getIdFactura() {
+        return idFactura;
+    }
+
+    public void setIdFactura(int idFactura) {
+        this.idFactura = idFactura;
+    }
+
+    public String getFechaFactura() {
+        return fechaFactura;
+    }
+
+    public void setFechaFactura(String fechaFactura) {
+        this.fechaFactura = fechaFactura;
+    }
+
+    public int getCodCliente() {
+        return codCliente;
+    }
+
+    public void setCodCliente(int codCliente) {
+        this.codCliente = codCliente;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    
+    
+    
+}
