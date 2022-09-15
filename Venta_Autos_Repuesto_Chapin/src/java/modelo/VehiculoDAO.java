c34b81782ae2f5fe3a2726eec339f481fb9b3ac9
Ricardo Juárez <rjuarez-2021417@kinal.edu.gt>
2022-09-15 17:59:14 -0600

    Se agregó contenido al archivo VehiculoDAO en proyecto Ventas_Autos_Repuestos

--- Venta_Autos_Repuesto_Chapin/src/java/modelo/VehiculoDAO.java
+++ Venta_Autos_Repuesto_Chapin/src/java/modelo/VehiculoDAO.java
@@ -6,6 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
+import modelo.Vehiculo;
 
 public class VehiculoDAO {
     Conexion conect = new Conexion();
