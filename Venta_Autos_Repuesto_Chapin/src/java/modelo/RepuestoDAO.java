e34b63365a8b33f17fa0b5f97534069d22fc53c5
Ricardo Juárez <rjuarez-2021417@kinal.edu.gt>
2022-09-15 17:58:09 -0600

    Se agregó contenido al archivo RepuestoDAO en proyecto Ventas_Autos_Repuestos

--- Venta_Autos_Repuesto_Chapin/src/java/modelo/RepuestoDAO.java
+++ Venta_Autos_Repuesto_Chapin/src/java/modelo/RepuestoDAO.java
@@ -6,6 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
+import modelo.Repuesto;
 
 public class RepuestoDAO {
     Conexion conect = new Conexion();
