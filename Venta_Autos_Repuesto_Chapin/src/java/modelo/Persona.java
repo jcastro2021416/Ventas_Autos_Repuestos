6ae08a089a7dc4aef230e1da6aa3c601e7f91df4
Dennis Arriaza <darriaza-2021432@kinal.edu.gt>
2022-09-20 12:35:51 -0600

    Se medifico Persona

--- Venta_Autos_Repuesto_Chapin/src/java/modelo/Persona.java
+++ Venta_Autos_Repuesto_Chapin/src/java/modelo/Persona.java
@@ -2,5 +2,44 @@
 package modelo;
 
 public class Persona {
+    int id;
+    String nom;
+    String usuario;
+    
+    public Persona() {
+        
+    }
+    
+    public Persona(int id, String nom, String correo) {
+       this.id = id;
+       this.nom = nom;
+       this.usuario = usuario;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+    
+    
     
 }
