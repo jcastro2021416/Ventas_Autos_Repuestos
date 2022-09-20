d3f9ffe1d17a4cc2dc41aefa414898d02641163f
Dennis Arriaza <darriaza-2021432@kinal.edu.gt>
2022-09-20 12:45:07 -0600

    Se modifico Controlador

--- Venta_Autos_Repuesto_Chapin/src/java/controlador/Controlador.java
+++ Venta_Autos_Repuesto_Chapin/src/java/controlador/Controlador.java
@@ -11,38 +11,38 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import modelo.Persona;
+import modelo.PersonaDAO;
 
 /**
  *
  * @author arria
  */
 public class Controlador extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    PersonaDAO dao=new PersonaDAO();
+    Persona p=new Persona();
+    int r;
+    
+    
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet Controlador</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet Controlador at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+       String accion=request.getParameter("accion");
+       if(accion.equals("Ingresar")){
+           String nom=request.getParameter("txtnom");
+           String usuario=request.getParameter("txtUsuario");
+           p.setNom(nom);
+           p.setUsuario(usuario);
+           dao.validar(p);
+           if(r==1){
+               request.getRequestDispatcher("Principal.jsp").forward(request, response);
+           }else{
+               request.getRequestDispatcher("index.jsp").forward(request, response);
+               
+       }
     }
+}
+
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
