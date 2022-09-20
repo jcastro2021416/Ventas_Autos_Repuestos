537762ee99cb754be5c906644fef125ae753f3a5
Dennis Arriaza <darriaza-2021432@kinal.edu.gt>
2022-09-20 12:41:33 -0600

    Se cambio PersonaDAO

--- Venta_Autos_Repuesto_Chapin/src/java/modelo/PersonaDAO.java
+++ Venta_Autos_Repuesto_Chapin/src/java/modelo/PersonaDAO.java
@@ -1,6 +1,45 @@
 
 package modelo;
 
-public class PersonaDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class PersonaDAO implements Validar{
+    Connection con;
+    Conexion cn=new Conexion();
+    PreparedStatement ps;
+    ResultSet rs;
+
+    @Override
+    public int validar(Persona per) {
+       int r=0;
+        String sql="Select * from persona where Nombre=? and Usuario=?";
+        try{
+            con=cn.getConnection();
+            ps=con.prepareStatement(sql);
+            ps.setString(1, per.getNom());
+            ps.setString(2, per.getUsuario());
+            rs=ps.executeQuery();
+            while(rs.next()){
+                r=r+1;
+                per.setNom(rs.getString("Nombres"));
+                per.setUsuario(rs.getString("Usuario"));
+                
+            }
+            if(r==1){
+                return 1;   
+            }else{
+                return 0;
+            }
+            
+        }catch (Exception e) {
+            return 0;
+        }
+        
+ }
+
+} 
     
-}
+        
+   
