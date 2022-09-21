a39224daf87e1d011ffc71b94ea5df730f94890e
Dennis Arriaza <darriaza-2021432@kinal.edu.gt>
2022-09-21 01:04:18 -0600

    Se cambio conexion

--- Venta_Autos_Repuesto_Chapin/src/java/config/Conexion.java
+++ Venta_Autos_Repuesto_Chapin/src/java/config/Conexion.java
@@ -1,5 +1,5 @@
 
-package modelo;
+package config;
 
 import com.mysql.jdbc.Connection;
 import java.sql.DriverManager;
@@ -12,7 +12,7 @@ public class Conexion {
     public Connection Conexion(){
         try{
             Class.forName("com.mysql.jdbc.Driver");
-            conexion = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/DBVenta_Autos_Repuesto_Chapin?useSSL=false","Naruto", "001");
+            conexion = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/DBVenta_Autos_Repuesto_Chapin?useSSL=false","root", "admin");
         }catch(Exception e){
             e.printStackTrace();
             System.out.println("Error De Conexion");
@@ -25,6 +25,3 @@ public class Conexion {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
-    
-    
-
