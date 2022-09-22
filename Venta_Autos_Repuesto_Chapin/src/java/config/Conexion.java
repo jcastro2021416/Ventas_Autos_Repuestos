8f7b62395e90bbc656ef6d45dd23461c85775d13
Ricardo Juárez <rjuarez-2021417@kinal.edu.gt>
2022-09-21 21:15:06 -0600

    Se modificó archivo index en proyecto Ventas_Autos_Repuestos

--- Venta_Autos_Repuesto_Chapin/src/java/config/Conexion.java
+++ Venta_Autos_Repuesto_Chapin/src/java/config/Conexion.java
@@ -9,7 +9,7 @@ public class Conexion {
     public Conexion(){
         try{
             Class.forName("com.mysql.jdbc.Driver");
-            conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/BaseDeDatos?useSSL=false", "root", "contraseña");
+            conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/DBVenta_Autos_Repuesto_Chapin?useSSL=false", "root", "contraseña");
         }catch (Exception e){
             e.printStackTrace();
             System.out.println("La conexiòn no se puede establecer" + e.getMessage());
